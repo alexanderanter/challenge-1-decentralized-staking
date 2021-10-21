744f287297b0d84050f3a08d81c1495431beaead
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2021-10-21 07:44:54 +0100

    adding staking function and event

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -7,18 +7,38 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = now + 30 seconds;
+
+  mapping (address => uint256 ) public balances;
+
+  event Stake(address accountAddress, uint256 amount);
+
+
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
+
+
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
 
 
+    function stake() public payable returns (uint) {
+      balances[msg.sender] += msg.value;
+      
+      emit Stake(msg.sender, msg.value);
+
+      return balances[msg.sender];
+    }
+
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
 
+    function execute() public {
 
+    }
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
